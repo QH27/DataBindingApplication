2054c79bf0a4d6d396b07cabeae6ec8cdfa3ab1f
QH27 <2151060231@e.tlu.edu.vn>
2024-05-08 22:31:10 +0700

    add Person

--- app/src/main/java/com/example/databindingapplication/MainActivity.java
+++ app/src/main/java/com/example/databindingapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.databindingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Person p1 = new Person("Huy", "huy123@gmail.com");
+
+        activityMainBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+        activityMainBinding.setPerson(p1);
     }
 }
